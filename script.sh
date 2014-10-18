be3d105b2dd5a66b68c93d978150ef3cc29628dd
stuart yeates <syeates@gmail.com>
2014-10-18 22:28:31 +1300

    update

--- script.sh
+++ script.sh
@@ -28,7 +28,7 @@ zdomain="za zm zr zw"
 
 COUNTRYDOMAINS="${adomain} ${bdomain} ${cdomain} ${ddomain} ${edomain} ${fdomain} ${gdomain} ${hdomain} ${idomain} ${jdomain} ${kdomain} ${ldomain} ${mdomain} ${ndomain} ${odomain} ${pdomain} ${qdomain} ${rdomain} ${sdomain} ${tdomain} ${udomain} ${vdomain} ${wdomain} ${ydomain} ${zdomain}"
 
-OTHERDOMAINS="int"
+OTHERDOMAINS="int com org net edu gov mil arpa"
 
 for from in 0 50 100 150 200 250 300 350 400 450 500 550 600 650 700 750 1000 1050 1100 1150 1200 1250 1300 1350 1400 1450 1500 1550 1600 1650 1700 1750 1800 1850 1900 1950; do
     for code in ${COUNTRYDOMAINS} ; do

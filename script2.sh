9f1282b00c81310d7fa015829455a381100e975a
stuart yeates <syeates@gmail.com>
2014-10-21 22:35:54 +1300

    update

--- script2.sh
+++ script2.sh
@@ -4,8 +4,10 @@ LANGUAGES=en
 
 for l in ${LANGUAGES}; do
     for t1 in `cat topic-terms.en.utf8`; do
-	for t2 in `cat general-tech-terms.en.utf8`; do
-	    echo wget --restrict-file-names=windows  --default-page=index.php --user-agent="" --ignore-length   "http://scholar.google.co.nz/scholar?q=${t1}+${t2}&num=20&start=80&as_sdt=0,5"
+	for t2 in `cat general-tech-terms.en.utf8 dspace-terms.en.utf8 eprints-terms.en.utf8`; do
+	    for n in 0 20 40 80 100 120 140 160 180 200 220 240 260 280; do
+		echo wget --restrict-file-names=windows  --default-page=index.php --user-agent="" --ignore-length   "http://scholar.google.co.nz/scholar?q=${t1}+${t2}&num=20&start=${n}&as_sdt=0,5"
+	    done;
 	done;
     done;
 done

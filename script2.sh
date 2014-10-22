eca76470e7e05b6aa3821cb23e04c6e1fe1a92e7
stuart yeates <syeates@gmail.com>
2014-10-22 22:36:10 +1300

    update

--- script2.sh
+++ script2.sh
@@ -6,7 +6,7 @@ for l in ${LANGUAGES}; do
     for t1 in `cat topic-terms.en.utf8`; do
 	for t2 in `cat general-tech-terms.en.utf8 dspace-terms.en.utf8 eprints-terms.en.utf8`; do
 	    for n in 0 20 40 80 100 120 140 160 180 200 220 240 260 280; do
-		echo wget --restrict-file-names=windows  --default-page=index.php --user-agent="" --ignore-length   "http://scholar.google.co.nz/scholar?q=${t1}+${t2}&num=20&start=${n}&as_sdt=0,5"
+		echo "http://scholar.google.co.nz/scholar?q=${t1}+${t2}&num=20&start=${n}&as_sdt=0,5"
 	    done;
 	done;
     done;

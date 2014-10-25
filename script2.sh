d19c979d23883e6752bed6b590b2cffbfdd5b548
stuart yeates <syeates@gmail.com>
2014-10-26 12:36:39 +1300

    updates

--- script2.sh
+++ script2.sh
@@ -2,17 +2,18 @@
 
 LANGUAGES=en
 
-for l in ${LANGUAGES}; do
-    for t1 in `cat topic-terms.en.utf8`; do
-	for t2 in `cat general-tech-terms.en.utf8 dspace-terms.en.utf8 eprints-terms.en.utf8`; do
-	    for n in 0 20 40 80 100 120 140 160 180 200 220 240 260 280; do
-		echo "http://scholar.google.co.nz/scholar?q=${t1}+${t2}&num=20&start=${n}&as_sdt=0,5" >> scholar.urls.en
-		echo "http://scholar.google.co.nz/scholar?q=${t1}&num=20&start=${n}&as_sdt=0,5" >> scholar.urls.en
-		echo "http://scholar.google.co.nz/scholar?q=${t2}&num=20&start=${n}&as_sdt=0,5" >> scholar.urls.en
-	    done;
-	done;
-    done;
-done
+#turns out not to be very helpful
+#for l in ${LANGUAGES}; do
+#    for t1 in `cat topic-terms.en.utf8`; do
+#	for t2 in `cat general-tech-terms.en.utf8 dspace-terms.en.utf8 eprints-terms.en.utf8`; do
+#	    for n in 0 20 40 80 100 120 140 160 180 200 220 240 260 280; do
+#		echo "http://scholar.google.co.nz/scholar?q=${t1}+${t2}&num=20&start=${n}&as_sdt=0,5" >> scholar.urls.en
+#		echo "http://scholar.google.co.nz/scholar?q=${t1}&num=20&start=${n}&as_sdt=0,5" >> scholar.urls.en
+#		echo "http://scholar.google.co.nz/scholar?q=${t2}&num=20&start=${n}&as_sdt=0,5" >> scholar.urls.en
+#	    done;
+#	done;
+#    done;
+#done
 
 for l in ${LANGUAGES}; do
     for t1 in `cat topic-terms.en.utf8`; do

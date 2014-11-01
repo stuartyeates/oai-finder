e0aebf275b6877855a9f54e0ef6bc15de917a79a
stuart yeates <syeates@gmail.com>
2014-11-01 23:13:45 +1300

    update

---
+++ yandex.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget --page-requisites --load-cookies yandex.cookies --save-cookies  yandex.cookies --keep-session-cookies --no-clobber --restrict-file-names=windows --default-page=index.php --user-agent="Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)" --ignore-length --wait 150 --force-directories --input-file=yandex.urls
+

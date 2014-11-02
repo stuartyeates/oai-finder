9e4adc114722c832498ef2531d32e44ca72bea43
stuart yeates <syeates@gmail.com>
2014-11-02 21:29:25 +1300

    update

--- yandex.sh
+++ yandex.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-wget --page-requisites --load-cookies yandex.cookies --save-cookies  yandex.cookies --keep-session-cookies --no-clobber --restrict-file-names=windows --default-page=index.php --user-agent="Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)" --ignore-length --wait 150 --force-directories --input-file=yandex.urls
+#wget --page-requisites --save-cookies  yandex.cookies --keep-session-cookies --no-clobber --restrict-file-names=windows --default-page=index.php --user-agent="Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/37.0.2062.120 Chrome/37.0.2062.120 Safari/537.36" --ignore-length --wait 150 --force-directories https://www.yandex.com/
+
+wget --page-requisites --load-cookies yandex.cookies --save-cookies  yandex.cookies --keep-session-cookies --no-clobber --restrict-file-names=windows --default-page=index.php --user-agent="Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/37.0.2062.120 Chrome/37.0.2062.120 Safari/537.36" --ignore-length --wait 150 --force-directories --input-file=yandex.urls
 

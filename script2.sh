f0b7e462db5d19c03acd114d230410ef3a8c3673
stuart yeates <syeates@gmail.com>
2014-11-06 21:43:27 +1300

    updates

--- script2.sh
+++ script2.sh
@@ -15,14 +15,15 @@ LANGUAGES=en
 #    done;
 #done
 
+
+######### Google 
 rm  google.urls
 
-
+#check for the most distinctive and language-independent search-terms we have across all active top level domains
 wget http://data.iana.org/TLD/tlds-alpha-by-domain.txt
 cat tlds-alpha-by-domain.txt | tr 'A-Z' 'a-z' | grep -v \# > all-domains
 DOMAINS=`cat all-domains`
 OAITERMS=`cat oai-terms.utf8`
-
 
 for from in 0 50; do
     for domain in ${DOMAINS} ; do
@@ -32,13 +33,18 @@ for from in 0 50; do
     done
 done
 
+
+#check for repository-specific search terms in great depth
 for language in ${LANGUAGES}; do
-    for t1 in `cat ojs-terms.en.utf8 islandora-terms.en.utf8 etd-db-terms.en.utf8 vital-terms.en.utf8 dspace-terms.${language}.utf8 eprints-terms.${language}.utf8`; do
+    for t1 in `cat  oai-terms.utf8 ojs-terms.${language}.utf8 islandora-terms.${language}.utf8 etd-db-terms.${language}.utf8 vital-terms.${language}.utf8 dspace-terms.${language}.utf8 eprints-terms.${language}.utf8`; do
 	for n in 0 50 100 150 200 250 300 350 400 450 500 550 600 650 700 750 800 850 900 950 ; do
 	    echo "https://www.google.co.nz/search?start=${n}&num=50&filter=0&q=${t1}" >> google.urls
 	done;
     done;
 done
+
+
+
 
 #ONEHUNDRED="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 2 526 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99"
 #
@@ -47,3 +53,22 @@ done
 #	echo "https://www.yandex.com/yandsearch?lr=87&text=${t2}&p=${n}" >> yandex.urls
 #    done
 #done
+
+
+######### Google 
+rm  microsoft.urls
+
+for n in 1 51 101 151 201 251 301 351 401 451 501 551 601 651 701 751 801 851 901 951 ; do
+    echo "https://www.bing.com/search?q=\"verb%3didentify\"+OAI-PMH&count=50&first=${n}"  >> microsoft.urls
+done;
+
+#check for repository-specific search terms in great depth
+for language in ${LANGUAGES}; do
+    for t1 in `cat  oai-terms.utf8 ojs-terms.${language}.utf8 islandora-terms.${language}.utf8 etd-db-terms.${language}.utf8 vital-terms.${language}.utf8 dspace-terms.${language}.utf8 eprints-terms.${language}.utf8`; do
+	for n in 1 51 101 151 201 251 301 351 401 451 501 551 601 651 701 751 801 851 901 951 ; do
+	    echo "https://www.bing.com/search?q=${t1}&count=50&first=${n}"  >> microsoft.urls
+	done;
+    done;
+done
+
+

6a7186c4c397bc6f0a4505007d70bd7060a4a8a1
stuart yeates <syeates@gmail.com>
2014-12-03 21:39:01 +1300

    pretty much right

--- find_candidate_urls.sh
+++ find_candidate_urls.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 
+find build*/downloads/ cache/google/ cache/bing/ cache/sogou/ -type f -exec /bin/cat \{\} \; |  tr '"<>()%,; ' '\012' | sed 's/http/\nhttp/' | grep http | grep -v 'google' | uniq | sort | uniq > build/raw_urls

352fdca359915324ceb613a4815bee5df2f9ca3c
stuart yeates <syeates@gmail.com>
2014-12-04 21:16:10 +1300

    a bit of reporting

--- find_candidate_urls.sh
+++ find_candidate_urls.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 find build*/downloads/ cache/google/ cache/bing/ cache/sogou/ -type f -exec /bin/cat \{\} \; |  tr '"<>()%,; ' '\012' | sed 's/http/\nhttp/' | grep http | grep -v 'google' | uniq | sort | uniq > build/raw_urls
+wc build/raw_urls

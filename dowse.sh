b71611cbc4357719fc451af24256581cb89d7378
stuart yeates <syeates@gmail.com>
2014-12-10 22:47:39 +1300

    find OAI URLs in stream

---
+++ dowse.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+CACHEDIR=./cache
+BUILDDIR=./build
+
+GUESSES=${BUILDDIR}/round1
+EXPLORE=${BUILDDIR}/explore
+
+rm ${GUESSES} ${EXPLORE}
+
+#cat downloads/*/* | tr ' "' '\012' |tr "'()<>" '\012' | grep // | grep '^http'| grep -v google | sort | uniq > ./raw.urls
+
+
+for url in `cat ${BUILDDIR}/raw_urls`; do 
+
+    if [[ $url == *verb\=Identify ]]
+    then
+	echo "${url}" >>  ${GUESSES}
+    fi
+
+    if [[ $url == */oai ]]
+    then
+	echo "${url}?verb=Identify" >>  ${GUESSES}
+	echo "${url}/request?verb=Identify" >>  ${GUESSES}
+    fi
+
+    if [[ $url == */oai/request ]]
+    then
+	echo "${url}?verb=Identify" >>  ${GUESSES}
+    fi
+
+    if [[ $url =~ .*/index.php/[^/]+/.* ]]
+    then
+	echo ${url} | sed 's|\(/index.php/[^/]*/\).*|\1oai?verb=Identify|' >>   ${GUESSES}
+	echo ${url} | sed 's|\(/index.php/\).*|\1|' >>  ${EXPLORE}
+    else
+	if [[ $url =~ .*/[oO][Jj][Ss]/.* ]]
+	then
+	    echo ${url} | sed 's|\(/[oO][Jj][Ss]/\).*|\1|' >>   ${EXPLORE}
+	else 
+	    if [[ $url =~ .*/[oO][Jj][Ss].* ]]
+	    then
+		echo ${url} | sed 's|\(/[oO][Jj][Ss]\).*|\1|' >>   ${GUESSES}
+	    else 
+		if [[ $url =~ .*/community-list ]]
+		then
+#		    echo ${url}
+#		    echo ${url} | sed 's|\(^.*\)/community-list|\1|'
+		    echo ${url} | sed 's|\(^.*\)/community-list|\1|' >>   ${GUESSES}
+		    
+		else 
+		    if [[ $url =~ .*/cgi-bin/library.cgi.* ]]
+		    then
+#		    echo ${url}
+#		    echo ${url} | sed 's|\(.*/cgi-bin\).*|\1/oaiserver.cgi?verb=Identify|'
+		    echo ${url} | sed 's|\(.*/cgi-bin\).*|\1/oaiserver.cgi?verb=Identify|' >>   ${GUESSES}
+			
+		    else 
+			if [[ $url =~ .*/[oO][Jj][Ss].* ]]
+			then
+			    echo ${url} | sed 's|\(/[oO][Jj][Ss]\).*|\1|' >>   ${GUESSES}
+			    
+			    
+			else 
+			    if [[ $url =~ .*/[oO][Jj][Ss].* ]]
+			    then
+				echo ${url} | sed 's|\(/[oO][Jj][Ss]\).*|\1|' >>   ${GUESSES}
+				
+				
+			    else 
+				if [[ $url =~ .*/[oO][Jj][Ss].* ]]
+				then
+				    echo ${url} | sed 's|\(/[oO][Jj][Ss]\).*|\1|' >>   ${GUESSES}
+				    
+				fi
+				
+			    fi
+			    
+			fi
+			
+		    fi
+		    
+		fi
+		
+	    fi
+	    
+	    
+	fi
+    fi
+    
+    
+done

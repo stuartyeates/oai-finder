b2018343dcdd4e408922ee02055c011a6c5b6a28
stuart yeates <syeates@gmail.com>
2014-12-11 20:08:26 +1300

    much faster grep

--- find_candidate_urls.sh
+++ find_candidate_urls.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find build*/downloads/ cache*/google/ cache*/bing/ cache*/sogou/ -type f -exec /bin/cat \{\} \;  |  tr '"<>()%, ' '\012' | sed 's/http/\nhttp/' |sed 's/=related:/\n/' | grep '^\(http\|[a-z]+\.[a-z]+/\)' | grep -v 'google.com' | grep -v 'msn.com' | grep -v 'ae.webradar.me' | grep -v 'ameinfo.com' | grep -v 'http://arab.jinbo.net/' | grep -v 'sogou.com' |grep -v 'live.com' | grep -v googleusercontent.com |grep -v google.co.nz  |sort | uniq > build/raw_urls
+find build*/downloads/ cache*/google/ cache*/bing/ cache*/sogou/ -type f -exec /bin/cat \{\} \;  |  tr '"<>()%, ' '\012' | sed 's/http/\nhttp/' |sed 's/=related:/\n/' | grep '^\(http\|[a-z]+\.[a-z]+/\)' | grep -Ev 'google.com|msn.com|ae.webradar.me|ameinfo.com|arab.jinbo.net|sogou.com|live.com|googleusercontent.com|google.co.nz'  |sort | uniq > build/raw_urls
 
 wc build/raw_urls

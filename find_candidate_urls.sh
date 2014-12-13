fb25e2fd703f38507c7ed9691015c8837cad622c
stuart yeates <syeates@gmail.com>
2014-12-13 22:12:56 +1300

    dont throw results away

--- find_candidate_urls.sh
+++ find_candidate_urls.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-find build*/downloads/ cache*/google/ cache*/bing/ cache*/sogou/ -type f -exec /bin/cat \{\} \;  |  tr '"<>()%, ' '\012' | sed 's/http/\nhttp/' |sed 's/=related:/\n/' | grep '^\(http\|[a-z]+\.[a-z]+/\)' | grep -Ev 'google.com|msn.com|ae.webradar.me|ameinfo.com|arab.jinbo.net|sogou.com|live.com|googleusercontent.com|google.co.nz'  |sort | uniq > build/raw_urls
+RAW=build/raw_urls
 
-wc build/raw_urls
+if [ -d ./${RAW} ]; then
+    mv ./${RAW} ./${RAW}-$(date -d "today" +"%Y%m%d%H%M%s")
+fi
+
+find build*/downloads/ cache*/google/ cache*/bing/ cache*/sogou/ -type f -exec /bin/cat \{\} \;  |  tr '"<>()%, ' '\012' | sed 's/http/\nhttp/' |sed 's/=related:/\n/' | grep '^\(http\|[a-z]+\.[a-z]+/\)' | grep -Ev 'google.com|msn.com|ae.webradar.me|ameinfo.com|arab.jinbo.net|sogou.com|live.com|googleusercontent.com|google.co.nz'  |sort | uniq > $RAW
+
+wc $RAW

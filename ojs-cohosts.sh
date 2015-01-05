1028da151a60dc94826c86d36cfc3194bd7a8ea5
stuart yeates <syeates@gmail.com>
2015-01-05 21:43:26 +1300

    better download of related ojs sites

--- ojs-cohosts.sh
+++ ojs-cohosts.sh
@@ -2,6 +2,6 @@
 for url in `cat build/explore  | sort | uniq`; 
 do  
 #    (wget "${url}" --no-clobber  --wait=5 --restrict-file-names=windows --directory-prefix=build/ojs --force-directories  --recursive --level=2 --convert-links --span-hosts --quiet &); 
-    (wget "${url}"  --restrict-file-names=windows --directory-prefix=build/ojs --force-directories  --convert-links --span-hosts -e robots=off --quiet &); 
+    (wget "${url}"  --recursive --level=1 --restrict-file-names=windows --directory-prefix=build/ojs --force-directories  --convert-links --span-hosts -e robots=off --quiet --wait=2 --random-wait &); 
     sleep 1; 
 done

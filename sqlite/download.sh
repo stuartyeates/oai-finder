2d53ee80bddf4f9cfc419985fc51521724932f95
stuart yeates <syeates@gmail.com>
2015-01-08 22:42:27 +1300

    now working ish

--- sqlite/download.sh
+++ sqlite/download.sh
@@ -2,25 +2,34 @@
 
 source ./issn.sh
 
-BUILD=../build
+BUILD=./build
 mkdir -p ${BUILD}
 
-cat ../build/guesses2 | shuf | head -100 > ./guesses
+GUESSES=./guesses
+cat ../build/guesses2 | shuf | head -3 | grep -v '^VITAL' > ${GUESSES}
 
 DB=${BUILD}/robotstxt
-rm -f ${DB}
 
-sqlite3 ${DB} "create table downloads (key INTEGER PRIMARY KEY, url TEXT, time INTEGER, status INTEGER, page TEXT);"
+MAX_FILE_LENGTH=50000
 
-NOW=`date +%s`
+if [ ! -e "${DB}" ]; 
+then
+    sqlite3 ${DB} "create table downloads (key INTEGER PRIMARY KEY, url TEXT, time INTEGER, status INTEGER, page TEXT);" 
+fi
+
+NOW=`date +%s%N`
 URL="http://www.victoria.ac.nz/robots.txt"
-PAGE=$(curl --fail ${URL})
+PAGE=$(curl --fail --max-time 20 ${URL})
 STATUS=$?
 sqlite3 ${DB} "insert into downloads (url, time, status, page) values ('${URL//\'/''}', '${NOW}', '${STATUS}', '${PAGE//\'/''}' );"
+sqlite3 ${DB} "select time, status, url from  downloads;"
+sqlite3 ${DB} "delete  from downloads where url = '${URL}';"
 
-sqlite3 ${DB} "select * from  downloads;"
 
-# function e {
-#     echo $1 
-# }  
-# e Hello
+for URL in `cat ${GUESSES} | sort | uniq | shuf`; do
+    echo ${URL}
+    PAGE=$(curl --fail ${URL})
+    STATUS=$?
+    PAGE2=`echo ${PAGE} |cut -c 1-${MAX_FILE_LENGTH}`
+    sqlite3 ${DB} "insert into downloads (url, time, status, page) values ('${URL//\'/''}', '${NOW}', '${STATUS}', '${PAGE2//\'/''}' );"
+done

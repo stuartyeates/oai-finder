b4499d0a106a8bbe199c9bda5c2e8dbf3a62ffec
stuart yeates <syeates@gmail.com>
2015-01-09 20:10:35 +1300

    ojs work

--- ojs-cohosts.sh
+++ ojs-cohosts.sh
@@ -5,6 +5,6 @@
 for url in `cat build/explore  | sort | uniq`; 
 do  
 #    (wget "${url}" --no-clobber  --wait=5 --restrict-file-names=windows --directory-prefix=build/ojs --force-directories  --recursive --level=2 --convert-links --span-hosts --quiet &); 
-    (wget "${url}"  --recursive --level=1 --restrict-file-names=windows --directory-prefix=build/ojs --force-directories  --convert-links --span-hosts -e robots=off --quiet --wait=2 --random-wait &); 
+    (wget "${url}"  --recursive --level=1 --restrict-file-names=windows --directory-prefix=build/ojs --force-directories  --convert-links --span-hosts -e robots=off --quiet --wait=2 --random-wait  -R mpg,mpeg,au,mp4,pdf,jpg,jpeg,png,gif,doc,docx &); 
     sleep 1; 
 done

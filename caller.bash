72bfc4eb50638587d612fa8d32359d360ce4fea1
stuart yeates <syeates@gmail.com>
2016-02-17 16:42:27 +1300

    update

--- caller.bash
+++ caller.bash
@@ -15,9 +15,10 @@ THIRD=""
 ENGINE=""
 ALGO="DEFAULT"
 
-while getopts ":sSr:d:" opt; do
+
+while getopts "a:e:1:2:3:r:d:f:" opt; do
     case $opt in
-	e)
+	a)
 	    ALGO=$OPTARG
 	    echo ALGO=$ALGO
 	    ;;
@@ -93,6 +94,77 @@ echo $USERAGENT
 echo ${RANDOM}
 echo ${COOKIEJAR}
 
+engine_google () {
+    #make sure our cache directory is created
+    mkdir -p "${CACHEDIR}/google/"
+    
+    if [  "$FIRST" ]
+    then
+	if [  "$SECOND" ]
+	then
+	    if [  "$THIRD" ]
+	    then
+		BASEURL="http://www.google.co.nz/search?q=${1}+${2}+${3}&filter=0&num=50"
+		CACHEBASE="${CACHEDIR}/google/${1}+${2}+${3}"
+	    else
+		BASEURL="http://www.google.co.nz/search?q=${1}+${2}&filter=0&num=50"
+		CACHEBASE="${CACHEDIR}/google/${1}+${2}"
+	    fi
+	else
+	    BASEURL="http://www.google.co.nz/search?q=${1}&filter=0&num=50"
+	    CACHEBASE="${CACHEDIR}/google/${1}"
+	fi
+    else
+	echo "Error: google_search called without args"
+	exit 1;
+    fi
 
+    #check to make sure we've not done this one before.
+    if [ -f "${CACHEBASE}.0.result" ];
+    then
+	echo "File "${CACHEBASE}.0.result" exists."
+    else
+	for n in 0 50 100 150 200 250 300 350 400 450 500 550 600 650 700 750 800 850 900 950 ; do
+	    sleep $INTRASEARCHPAUSE
+	    echo doing "${BASEURL}" ${n}
+	    curl --max-time 30  --cookie-jar "${COOKIEJAR}.google" --dump-header "${CACHEBASE}.${n}.header" --output "${CACHEBASE}.${n}.result" --stderr "${CACHEBASE}.${n}.logging" --referer "http://www.google.com/" --verbose -A "${USERAGENT}" --url "${BASEURL}&start=${n}"
+	done;
+    fi
+    
+}
 
+main() {
+    case "${ALGO}" in
+        SEED) seed_files
+              ;;
+        FIELD_SEARCH) search_by_field
+			   ;;
+        SOFTWARE_SEARCH) search_by_software
+			 ;;
+        URL_SEARCH) search_from_url
+		;;
+        EXTEND) search_from_previous
+		;;
+	DEFAULT)
+            *) search_by_software
+            ;;
+   esac
+exit 0;
+}
 
+ 
+ search_by_field () {
+     echo "searching by field";
+ }
+
+ search_by_software () {
+     echo "searching by software";
+ }
+
+ search_from_url () {
+     echo "searching from url";
+ }
+ 
+  search_from_previous () {
+      echo "searching from prevoious";
+ }

b413dcf7ff53419c6fc7977a3d85c3e9e4c40dab
stuart yeates <syeates@gmail.com>
2016-03-07 15:51:55 +1300

    add

--- doaj.bash
+++ doaj.bash
@@ -7,11 +7,11 @@ mkdir -p ${CACHE_DIR}
 OPENDOARJSONFILE=${CACHE_DIR}/opendoaj.xml
 
 function doaj () {
-    END=20
+    END=200
     
     for count in $(seq 1 $END); do
 
-	sleep 10
+	sleep 2
 	echo $i;
 	curl -X GET --header "Accept: application/json" "https://doaj.org/api/v1/search/journals/http?page=${count}&pageSize=100" --output "${OPENDOARJSONFILE}.${count}"
 	if [ $? -ne 0 ]

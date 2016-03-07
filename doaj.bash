9d8b0256f69c3e0eaa06183c6912830876eb443d
stuart yeates <syeates@gmail.com>
2016-03-07 15:49:47 +1300

    add

--- doaj.bash
+++ doaj.bash
@@ -1,46 +1,28 @@
 #!/bin/bash
 
 
-CACHE_DIR=./cache-dir/opendoar/
+CACHE_DIR=./cache-dir/opendoaj/
 mkdir -p ${CACHE_DIR}
 
-OPENDOARXMLFILE=${CACHE_DIR}/opendoar.xml
-OPENDOARCSVFILE=${CACHE_DIR}/opendoar.csv
+OPENDOARJSONFILE=${CACHE_DIR}/opendoaj.xml
 
-function opendoar () {
-    echo doing "${OPENDOARXMLFILE}"
-    # make sure we have the xml file
-    if [ ! -f "${OPENDOARXMLFILE}" ]; then
-	 curl --max-time 300 --output "${OPENDOARXMLFILE}"  --referer "http://www.google.com/" --stderr "${CACHE_DIR}/main.log" --verbose  --url "http://opendoar.org/api13.php?all=y"
-    fi
-
-    #convert the XML file to a CSV, with just the URLs plus the repositry name
-    xsltproc ./opendoar.xsl cache-dir/opendoar/opendoar.xml > ${OPENDOARCSVFILE}
+function doaj () {
+    END=20
     
-    #split the lines into feeds and hints
+    for count in $(seq 1 $END); do
 
-    #first nuke the old ones
-    rm -f ${CACHE_DIR}/probably_oai_feeds ${CACHE_DIR}/oai_feed_hints ${CACHE_DIR}/oai_feed_domains
-    
-    OLDIFS=$IFS
-    IFS=,
-
-    while read rUrl rOaiBaseUrl oUrl 
-    do
-	if [ ! -z "${rOaiBaseUrl}" ]; then
-	    echo "${rOaiBaseUrl}" >> ${CACHE_DIR}/probable_oai_feeds
-	else
-	    if [ ! -z "${rUrl}" ]; then
-		echo "${rUrl}" >> ${CACHE_DIR}/oai_feed_hints
-	    else
-		if [ ! -z "${oUrl}" ]; then
-		    echo "${oUrl}" >> ${CACHE_DIR}/oai_feed_domains
-		fi
-	    fi
+	sleep 10
+	echo $i;
+	curl -X GET --header "Accept: application/json" "https://doaj.org/api/v1/search/journals/http?page=${count}&pageSize=100" --output "${OPENDOARJSONFILE}.${count}"
+	if [ $? -ne 0 ]
+	then
+	    break;
 	fi
-    done < ${OPENDOARCSVFILE}
-    IFS=$OLDIFS
+	
+    done
+    
+    
 }
 
 
-opendoar;
+doaj;

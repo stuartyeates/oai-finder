7261d0995b63be6a38aa6281e6cb857fd9db6ed4
stuart yeates <syeates@gmail.com>
2016-03-09 22:44:31 +1300

    start regexp matching

--- opendoar.bash
+++ opendoar.bash
@@ -8,7 +8,7 @@ OPENDOARXMLFILE=${CACHE_DIR}/opendoar.xml
 OPENDOARCSVFILE=${CACHE_DIR}/opendoar.csv
 
 function opendoar () {
-    echo doing "${OPENDOARXMLFILE}"
+    #echo doing "${OPENDOARXMLFILE}"
     # make sure we have the xml file
     if [ ! -f "${OPENDOARXMLFILE}" ]; then
 	 curl --max-time 300 --output "${OPENDOARXMLFILE}"  --referer "http://www.google.com/" --stderr "${CACHE_DIR}/main.log" --verbose  --url "http://opendoar.org/api13.php?all=y"
@@ -47,9 +47,25 @@ PAIRSFILE=${CACHE_DIR}/pairs.csv
 
 function find_rep_oai_pairs (){
   awk -F, '{if ( $2) { print $1, $2} }' <  ${OPENDOARCSVFILE} > ${PAIRSFILE}
-  
 }
 
+function match_with_sed () {
+    
+
+    OLDIFS=$IFS
+    IFS=' ' 
+
+    while read rep oai 
+    do
+	G1=$(echo "$rep" | sed 's|$|cgi/oai2|');
+	G2=$(echo "$rep" | sed 's|$|oai|');
+	G3=$(echo "$rep" | sed 's|$|cgi/oai2|');
+	echo $rep, $oai, $G1, $G2, $G3
+	
+    done < ${PAIRSFILE}
+    IFS=$OLDIFS
+    }
 
 opendoar;
 find_rep_oai_pairs;
+match_with_sed;

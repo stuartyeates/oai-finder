2bea5123a7ad079ccbb7149e9988f58f4221529b
stuart yeates <syeates@gmail.com>
2016-03-13 22:47:06 +1300

    start mutator

--- mutate-host-part.bash
+++ mutate-host-part.bash
@@ -6,7 +6,10 @@ do
     if [ ! -z "${URL}" ]; then
 	echo $URL
 	echo $URL | perl -p -e  's!(http|https)://([^/]+)/!protocol=\1 host=\2 !g'
-	
-	
+
+	host 
+	if [ "$?" -eq "0" ]; then
+	    echo host 
+	fi
     fi
 done

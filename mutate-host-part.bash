eb67ce99f35559656b916d5372f86913e1d73d30
stuart yeates <syeates@gmail.com>
2016-03-15 09:49:23 +1300

    better host stuff

--- mutate-host-part.bash
+++ mutate-host-part.bash
@@ -4,10 +4,11 @@
 while read URL
 do
     if [ ! -z "${URL}" ]; then
-	echo $URL
-	echo $URL | perl -p -e  's!(http|https)://([^/]+)/!protocol=\1 host=\2 !g'
-
-	host 
+	echo ${URL}
+	echo ${URL} | perl -p -e  's!(http|https)://([^/]+)/!protocol=\1 host=\2 !g'
+	HOST=`echo ${URL} | perl -p -e  's!(http|https)://([^/]+)/.*!\2!g'`
+	echo HOST="${HOST}"
+	host "${HOST}"
 	if [ "$?" -eq "0" ]; then
 	    echo host 
 	fi

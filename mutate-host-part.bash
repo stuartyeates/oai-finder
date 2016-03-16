01154403680bd65355130f56bf90dc452d356576
stuart yeates <syeates@gmail.com>
2016-03-16 21:22:06 +1300

    better host stuff

--- mutate-host-part.bash
+++ mutate-host-part.bash
@@ -26,6 +26,31 @@ do
 	fi
 	
 	OAIHOST=`echo oai.${HOST}`
+	echo $OAIHOST
+	host ${OAIHOST} 1>&2 > /dev/null
+	if [ "$?" -eq "0" ]; then
+	    ALIASHOST=`echo ${URL} | perl -p -e 's!(http|https)://([^/]+)/(.*)!\1://'"${OAIHOST}"'/\3!g'`
+	    echo $ALIASHOST
+	fi
+	
+	OAIHOST=`echo ${HOST} | sed 's/^www.//'`
+	echo $OAIHOST
+	host ${OAIHOST} 1>&2 > /dev/null
+	if [ "$?" -eq "0" ]; then
+	    ALIASHOST=`echo ${URL} | perl -p -e 's!(http|https)://([^/]+)/(.*)!\1://'"${OAIHOST}"'/\3!g'`
+	    echo $ALIASHOST
+	fi
+	
+	OAIHOST=`echo ${HOST} | sed 's/^www./oai./'`
+	echo $OAIHOST
+	host ${OAIHOST} 1>&2 > /dev/null
+	if [ "$?" -eq "0" ]; then
+	    ALIASHOST=`echo ${URL} | perl -p -e 's!(http|https)://([^/]+)/(.*)!\1://'"${OAIHOST}"'/\3!g'`
+	    echo $ALIASHOST
+	fi
+	
+	OAIHOST=`echo ${HOST} | sed 's/^[^\.]\+\.//'`
+	echo $OAIHOST
 	host ${OAIHOST} 1>&2 > /dev/null
 	if [ "$?" -eq "0" ]; then
 	    ALIASHOST=`echo ${URL} | perl -p -e 's!(http|https)://([^/]+)/(.*)!\1://'"${OAIHOST}"'/\3!g'`

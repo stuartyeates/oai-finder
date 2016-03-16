c05495551a724092241e82919fb2c975fea552a4
stuart yeates <syeates@gmail.com>
2016-03-16 20:44:19 +1300

    better host stuff

--- mutate-host-part.bash
+++ mutate-host-part.bash
@@ -1,30 +1,27 @@
 #!/bin/bash
-
+#script to create variants of he host part of the URL
 
 while read URL
 do
     if [ ! -z "${URL}" ]; then
-	echo PASS ${URL}
-	echo ${URL} | perl -p -e  's!(http|https)://([^/]+)/!protocol=\1 host=\2 !g'
+	echo ${URL}
 	HOST=`echo ${URL} | perl -p -e  's!(http|https)://([^/]+)/.*!\2!g'`
-	echo HOST="${HOST}"
-	host "${HOST}"
 	if [ "$?" -eq "0" ]; then
 	    OUTPUT=`host ${HOST}`
 	    ADDRESS=`echo ${OUTPUT} | grep 'has address' | sed 's/.* has address \([^ ]*\).*/\1/' | grep -v address| head -1 `
 	    if [ ! -z "${ADDRESS}" ]; then
 		IPHOST=`echo ${URL} | perl -p -e 's!(http|https)://([^/]+)/(.*)!\1://'"${ADDRESS}"'/\3!g'`
-		echo PASS $IPHOST
+		echo $IPHOST
 	    fi
 	    ADDRESS=`echo ${OUTPUT} | grep 'has address' | sed 's/.* has IPv6 address \([^ ]*\).*/\1/' | grep -v addres| head -1 `
 	    if [ ! -z "${ADDRESS}" ]; then
 		IPHOST=`echo ${URL} | perl -p -e 's!(http|https)://([^/]+)/(.*)!\1://'"${ADDRESS}"'/\3!g'`
-		echo PASS $IPHOST
+		echo $IPHOST
 	    fi
 	    ADDRESS=`echo ${OUTPUT} | grep 'has address' | sed 's/.* is an alias for \([^ ]*\).*/\1/' | grep -v addres| head -1 `
 	    if [ ! -z "${ADDRESS}" ]; then
-		IPHOST=`echo ${URL} | perl -p -e 's!(http|https)://([^/]+)/(.*)!\1://'"${ADDRESS}"'/\3!g'`
-		echo PASS $IPHOST
+		ALIASHOST=`echo ${URL} | perl -p -e 's!(http|https)://([^/]+)/(.*)!\1://'"${ADDRESS}"'/\3!g'`
+		echo $ALIASHOST
 	    fi
 	fi
     fi

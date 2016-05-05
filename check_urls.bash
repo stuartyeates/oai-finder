f2dc410f03145dc5e927e994bd4ad6aab524d4fc
stuart yeates <syeates@gmail.com>
2016-05-05 22:01:21 +1200

    tweak

--- check_urls.bash
+++ check_urls.bash
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+mkdir -p ./logs
+mkdir -p ./tmp
+
+output=$(mktemp ./tmp/wget-output.XXXXXX)
+success=./logs/goodurls-$$
+failure=./logs/goodurls-$$
+
 while read url
 do
     echo ${url}
+    wget 
+    
 done

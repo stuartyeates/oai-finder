b9e559aeabc83a45b28aa61f76b19db9bec6f27d
stuart yeates <syeates@gmail.com>
2016-05-10 21:20:22 +1200

    tweaks

--- gen_cdm_urls.bash
+++ gen_cdm_urls.bash
@@ -8,6 +8,6 @@ mkdir ${DIR}
 
 for i in {1..50000}
 do
-    echo wget --force-directories --directory-prefix=${DIR} https://cdm${i}.contentdm.oclc.org/
+    wget --force-directories --directory-prefix=${DIR} https://cdm${i}.contentdm.oclc.org/ &
     sleep 1
 done

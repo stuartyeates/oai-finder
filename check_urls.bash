f7836ebac5c4c161758c9d3a1ef7dfcfff8ab24f
stuart yeates <syeates@gmail.com>
2016-05-15 10:58:53 +1200

    querying sites for the first time

--- check_urls.bash
+++ check_urls.bash
@@ -3,13 +3,31 @@
 mkdir -p ./logs
 mkdir -p ./tmp
 
-output=$(mktemp ./tmp/wget-output.XXXXXX)
-success=./logs/goodurls-$$
-failure=./logs/goodurls-$$
-
 while read url
 do
-    echo ${url}
-    wget 
+    dir=$(mktemp --directory ./tmp/wget-XXXXXXXXX)
+    #mkdir -p ${dir}
     
+    output=${dir}/output
+    log=${dir}/logs
+    result=${dir}/result
+    success=./logs/successes-$$
+    failure=./logs/failures-$$
+    
+    echo ${url}
+    echo ${url} > ${result}
+    wget ${url} --output-file=${log} --verbose --output-document=${output} --timeout=10 --tries=3  --no-check-certificate
+    RESULT=$?
+    echo ${RESULT} >> ${result}
+
+    grep '\(<OAI-PMH\|http://www.openarchives.org/OAI/2.0/\|<baseURL\)' ${output} >> /dev/null
+    GREP1=$?
+    
+    if [ "${GREP1}" -eq "0"  ]
+    then
+	echo ${url} >> ${success}
+	firefox "https://web.archive.org/save/${url}" &
+    else
+	echo ${url} >> ${failure}
+    fi
 done

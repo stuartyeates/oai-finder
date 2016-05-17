2fe3459a8c4f36e7cb48b1758e817f6c7718792f
stuart yeates <syeates@gmail.com>
2016-05-17 19:50:22 +1200

    better error handling

--- check_urls.bash
+++ check_urls.bash
@@ -5,29 +5,42 @@ mkdir -p ./tmp
 
 while read url
 do
-    dir=$(mktemp --directory ./tmp/wget-XXXXXXXXX)
-    #mkdir -p ${dir}
-    
-    output=${dir}/output
-    log=${dir}/logs
-    result=${dir}/result
-    success=./logs/successes-$$
-    failure=./logs/failures-$$
-    
-    echo ${url}
-    echo ${url} > ${result}
-    wget ${url} --output-file=${log} --verbose --output-document=${output} --timeout=10 --tries=3  --no-check-certificate
-    RESULT=$?
-    echo ${RESULT} >> ${result}
-
-    grep '\(<OAI-PMH\|http://www.openarchives.org/OAI/2.0/\|<baseURL\)' ${output} >> /dev/null
-    GREP1=$?
-    
-    if [ "${GREP1}" -eq "0"  ]
+    if [ -n "${url}" ] ;
     then
-	echo ${url} >> ${success}
-	firefox "https://web.archive.org/save/${url}" &
-    else
-	echo ${url} >> ${failure}
+	dir2=$(mktemp --directory ./tmp/tmp-XXXX)
+	dir1=$(mktemp --directory ${dir2}/XXXX)
+	dir=$(mktemp --directory ${dir1}/XXXX)
+	#    mkdir -p ${dir}
+	
+	output=${dir}/output
+	log=${dir}/logs
+	result=${dir}/result
+	success=./logs/successes-$$
+	error=./logs/errors-$$
+	failure=./logs/failures-$$
+    
+	echo ${url}
+	echo ${url} > ${result}
+	wget ${url} --output-file=${log} --verbose --output-document=${output} --timeout=10 --tries=3  --no-check-certificate
+	RESULT=$?
+	echo ${RESULT} >> ${result}
+	
+	grep '\(<OAI-PMH\|http://www.openarchives.org/OAI/2.0/\|<baseURL\)' ${output} >> /dev/null
+	GREP1=$?
+	
+	if [ "${RESULT}" -eq "4"  ]
+	then
+		echo ${url} >> ${error}	    
+	else
+	    if [ "${GREP1}" -eq "0"  ]
+	    then
+		echo ${url} >> ${success}
+		#firefox "https://web.archive.org/save/${url}" &
+	    else
+		echo ${url} >> ${failure}
+	    fi
+	    
+	fi
     fi
+	
 done
